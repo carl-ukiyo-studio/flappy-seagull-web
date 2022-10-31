0cde029c44aa9d234d989afec385caea57c649f6
Carl Meyer <carl@ukiyo-studio.co.uk>
2022-10-31 22:20:47 +0000

    GameHint Component

---
+++ src/components/GameHint.jsx
@@ -0,0 +1,14 @@
+const GameHint = () => (
+  <div className="block max-w-sm rounded-lg border border-gray-900 bg-gray-800 p-6 text-center shadow-md">
+    <h5 className="sm:text-l mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white">
+      You are a seagull trying to navigate the streets of Brighton City.
+    </h5>
+    <p className="text-sm font-normal text-gray-700 dark:text-gray-400">
+      Use mouse click or space bar to flap your wings and fly through
+      the city trying to avoid the obstacles in the way. See how many
+      points you can score!
+    </p>
+  </div>
+);
+
+export default GameHint;
