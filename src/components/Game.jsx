1d5414c4bf538370c4a3b3fcba2bccd9d76dbe25
Carl Meyer <carl@ukiyo-studio.co.uk>
2022-11-06 17:13:20 +0000

    Game Update 2
    - New Game files that fix 2 bugs
    - Added full screen button
    - removed debug stuff

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,10 +3,9 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase-config";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import FullScreenButton from "./FullScreenButton";
 
 const Game = () => {
-  const [isGameOver, setIsGameOver] = useState(false);
-  const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const navigate = useNavigate();
   const userName = localStorage.getItem("username");
@@ -18,6 +17,7 @@ const Game = () => {
     loadingProgression,
     addEventListener,
     removeEventListener,
+    requestFullscreen,
   } = useUnityContext({
     loaderUrl: "/game/FlappySeagull.loader.js",
     dataUrl: "/game/FlappySeagull.data.unityweb",
@@ -25,8 +25,35 @@ const Game = () => {
     codeUrl: "/game/FlappySeagull.wasm.unityweb",
   });
 
+  // We'll use a state to store the device pixel ratio.
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio
+  );
+
+  const handleChangePixelRatio = useCallback(
+    function () {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = function () {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+      return function () {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio]
+  );
+
   const getPlayer = async () => {
-    debugger;
     const docRef = doc(db, "players", userName);
     try {
       const docSnap = await getDoc(docRef);
@@ -44,7 +71,6 @@ const Game = () => {
   };
 
   const saveHighScore = (score) => {
-    debugger;
     if (score > highScore) {
       setHighScore(score);
       saveHighScoreToDatabase(score);
@@ -65,18 +91,23 @@ const Game = () => {
       });
   };
 
-  const handleGameOver = useCallback((score) => {
-    setIsGameOver(true);
-    setScore(score);
-    console.log(`Game Over ${userName}! You've scored ${score} points.`);
+  const handleGameOver = useCallback(async (score) => {
     saveHighScore(score);
     // eslint-disable-next-line
   }, []);
 
-  const handlePlayAgain = useCallback(() => {
-    setIsGameOver(false);
-    console.log(`Playing Again!`);
-  }, []);
+  useEffect(() => {
+    if (userName == null) {
+      goBackToHomePage();
+    }
+    getPlayer().then((r) => {
+      addEventListener("GameOver", handleGameOver);
+    });
+    return () => {
+      removeEventListener("GameOver", handleGameOver);
+    };
+    // eslint-disable-next-line
+  }, [addEventListener, removeEventListener, handleGameOver]);
 
   function goBackToHomePage() {
     navigate("/");
@@ -90,28 +121,21 @@ const Game = () => {
     };
   }, [detachAndUnloadImmediate]);
 
-  useEffect(() => {
-    if (userName == null) {
-      goBackToHomePage();
-    }
-    getPlayer().then((r) => {
-      addEventListener("GameOver", handleGameOver);
-      addEventListener("PlayAgain", handlePlayAgain);
-    });
-    return () => {
-      removeEventListener("GameOver", handleGameOver);
-      removeEventListener("PlayAgain", handlePlayAgain);
-    };
-    // eslint-disable-next-line
-  }, [addEventListener, removeEventListener, handleGameOver]);
-
   // We'll round the loading progression to a whole number to represent the
   // percentage of the Unity Application that has loaded.
   const loadingPercentage = Math.round(loadingProgression * 100);
 
+  const onFullScreen = () => {
+    requestFullscreen(true);
+  };
+
   return (
     <div className="container mx-auto">
-      <Unity className="w-full" unityProvider={unityProvider} />
+      <Unity
+        className="w-full"
+        unityProvider={unityProvider}
+        devicePixelRatio={devicePixelRatio}
+      />
       {isLoaded === false && (
         // We'll conditionally render the loading overlay if the Unity
         // Application is not loaded.
@@ -119,9 +143,9 @@ const Game = () => {
           <p>Loading... ({loadingPercentage}%)</p>
         </div>
       )}
-      {isGameOver === true && (
-        <div className="mt-4 flex justify-center bg-gray-800 py-4 align-middle text-2xl font-medium text-white">
-          <p>{`Game Over ${userName}! You've scored ${score} points.`}</p>
+      {isLoaded === true && (
+        <div className="flex w-auto justify-center py-4">
+          <FullScreenButton onFullScreen={onFullScreen}></FullScreenButton>
         </div>
       )}
     </div>
