55848ec85aa03512db487788eca53ede58953fd2
Carl Meyer <carl@ukiyo-studio.co.uk>
2022-11-06 17:15:43 +0000

    hotfix - lint

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -25,34 +25,6 @@ const Game = () => {
     codeUrl: "/game/FlappySeagull.wasm.unityweb",
   });
 
-  // We'll use a state to store the device pixel ratio.
-  const [devicePixelRatio, setDevicePixelRatio] = useState(
-    window.devicePixelRatio
-  );
-
-  const handleChangePixelRatio = useCallback(
-    function () {
-      // A function which will update the device pixel ratio of the Unity
-      // Application to match the device pixel ratio of the browser.
-      const updateDevicePixelRatio = function () {
-        setDevicePixelRatio(window.devicePixelRatio);
-      };
-      // A media matcher which watches for changes in the device pixel ratio.
-      const mediaMatcher = window.matchMedia(
-        `screen and (resolution: ${devicePixelRatio}dppx)`
-      );
-      // Adding an event listener to the media matcher which will update the
-      // device pixel ratio of the Unity Application when the device pixel
-      // ratio changes.
-      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
-      return function () {
-        // Removing the event listener when the component unmounts.
-        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
-      };
-    },
-    [devicePixelRatio]
-  );
-
   const getPlayer = async () => {
     const docRef = doc(db, "players", userName);
     try {
